ed90998a0d5cde41c696a4eec7d26ec454b286c5
matviu <matvi.u@yandex.ru>
2017-09-01 17:07:14 +0500

    добавил в арифметический тренажер выбор диапазона чисел

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task("serve", function() {
 
 gulp.task("copy", function() {
   return gulp.src([
-    "fonts/**/*.{woff,woff2}",
+    "fonts/**/*.{ttf,woff,woff2}",
     "img/**",
     "js/**",
     "*.html"
